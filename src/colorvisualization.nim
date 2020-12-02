51b55a0c9517d60a933d40b3385f1af6b621cdaa
Dmitry Matveyev <dm.matveyev@protonmail.com>
2020-12-02 06:46:02 +0300

    Improve tests and don't cut floating point values

--- src/colorvisualization.nim
+++ src/colorvisualization.nim
@@ -1,4 +1,4 @@
-from math import round, `mod`, pow, sqrt
+from math import `mod`, pow, sqrt
 from strformat import fmt
 from strutils import replace, HexDigits, toHex, parseHexInt
 
@@ -71,15 +71,6 @@ func toHex*(c: RGB): string =
     b = int(c.b * 256)
   fmt"#{toHex(r, 2)}{toHex(g, 2)}{toHex(b, 2)}"
 
-func `$`*(c: RGB): string =
-  fmt"{c.r:>3.1f}, {c.g:>3.1f}, {c.b:>3.1f}".replace(" ", "&nbsp;")
-
-func `$`*(c: HSL): string =
-  fmt"{c.h.float:>5.1f}°, {c.s.float:>3.1f}, {c.l.float:>3.1f}".replace(" ", "&nbsp;")
-
-func `$`*(c: Lab): string =
-  fmt"{c.l.float:>5.1f}, {c.a:>5.1f}, {c.b:>5.1f}".replace(" ", "&nbsp;")
-
 func hexToRGB*(s: string): RGB =
   if s.len != 7 or
      s[0] != '#' or
@@ -119,9 +110,6 @@ func toHSL*(c: RGB): HSL =
       elif maxValue == c.b: (c.r - c.g) / chroma + 4.0
       else: assert false; 0.0
     result.h *= 60
-    result.h = round(result.h, 1)
-    result.s = round(result.s, 3)
-    result.l = round(result.l, 3)
 
 # https://en.wikipedia.org/wiki/HSL_and_HSV#HSL_to_RGB_alternative
 func toRGB*(c: HSL): RGB =
@@ -129,11 +117,12 @@ func toRGB*(c: HSL): RGB =
     let k = (n + c.h / 30.0) mod 12
     c.l - c.s * min(c.l, 1 - c.l) * max(-1, min(k - 3, min(9 - k, 1.0)))
 
-  result.r = f(0.0).round(3)
-  result.g = f(8.0).round(3)
-  result.b = f(4.0).round(3)
+  result.r = f(0.0)
+  result.g = f(8.0)
+  result.b = f(4.0)
 
 # http://www.easyrgb.com/en/math.php
+# https://en.wikipedia.org/wiki/SRGB
 func toXYZ*(c: RGB): XYZ =
   var
     r = c.r.float
@@ -145,29 +134,27 @@ func toXYZ*(c: RGB): XYZ =
   r *= 100
   g *= 100
   b *= 100
-  result.x = r * 0.4124 + g * 0.3576 + b * 0.1805
-  result.y = r * 0.2126 + g * 0.7152 + b * 0.0722
-  result.z = r * 0.0193 + g * 0.1192 + b * 0.9505
-  result.x = round(result.x, 3)
-  result.y = round(result.y, 3)
-  result.z = round(result.z, 3)
+  result.x = r * 0.41239080 + g * 0.35758434 + b * 0.18048079
+  result.y = r * 0.21263901 + g * 0.71516868 + b * 0.07219232
+  result.z = r * 0.01933082 + g * 0.11919478 + b * 0.95053215
   result.wp = WhitePoint.d65
 
 # http://www.easyrgb.com/en/math.php
+# https://en.wikipedia.org/wiki/SRGB
 func toRGB*(c: XYZ): RGB =
   var
     x = c.x / 100.0
     y = c.y / 100.0
     z = c.z / 100.0
-    r = x *  3.2406 + y * -1.5372 + z * -0.4986
-    g = x * -0.9689 + y *  1.8758 + z *  0.0415
-    b = x *  0.0557 + y * -0.2040 + z *  1.0570
+    r = x *  3.24096994 + y * -1.53738318 + z * -0.49861076
+    g = x * -0.96924364 + y *  1.87596750 + z *  0.04155506
+    b = x *  0.05563008 + y * -0.20397696 + z *  1.05697151
   r = if r > 0.0031308: 1.055 * pow(r, (1 / 2.4)) - 0.055 else: 12.92 * r
   g = if g > 0.0031308: 1.055 * pow(g, (1 / 2.4)) - 0.055 else: 12.92 * g
   b = if b > 0.0031308: 1.055 * pow(b, (1 / 2.4)) - 0.055 else: 12.92 * b
-  result.r = r.round(3)
-  result.g = g.round(3)
-  result.b = b.round(3)
+  result.r = clamp(r, 0.0, 1.0)
+  result.g = clamp(g, 0.0, 1.0)
+  result.b = clamp(b, 0.0, 1.0)
 
 # http://www.easyrgb.com/en/math.php
 func toLab*(c: XYZ): Lab =
@@ -182,9 +169,6 @@ func toLab*(c: XYZ): Lab =
   result.l = 116 * y - 16
   result.a = 500 * (x - y)
   result.b = 200 * (y - z)
-  result.l = round(result.l, 3)
-  result.a = round(result.a, 3)
-  result.b = round(result.b, 3)
 
 # http://www.easyrgb.com/en/math.php
 func toXYZ*(c: Lab): XYZ =
@@ -203,9 +187,6 @@ func toXYZ*(c: Lab): XYZ =
   result.x = x * rv.x
   result.y = y * rv.y
   result.z = z * rv.z
-  result.x = round(result.x, 3)
-  result.y = round(result.y, 3)
-  result.z = round(result.z, 3)
 
 # http://www.easyrgb.com/en/math.php
 # Euclidian distance
@@ -214,7 +195,7 @@ func deltaE*(m, p: Lab): float =
     ld = m.l - p.l
     ad = m.a - p.a
     bd = m.b - p.b
-  sqrt(ld*ld + ad*ad + bd*bd).round(1)
+  sqrt(ld*ld + ad*ad + bd*bd)
 
 func deltaE*(m, p: RGB): float = deltaE(m.toXYZ.toLab, p.toXYZ.toLab)
 
@@ -233,7 +214,7 @@ func contrastRatio(m, p: RGB): float =
   result =
     if luminance1 > luminance2: (luminance1 + 0.05) / (luminance2 + 0.05)
     else: (luminance2 + 0.05) / (luminance1 + 0.05)
-  result = result.round(1)
+  result = result
 
 
 
@@ -243,6 +224,15 @@ func contrastRatio(m, p: RGB): float =
 
 when isMainModule:
   import dom
+
+  func htmlize(c: RGB): string =
+    fmt"{c.r.float:>3.1f}, {c.g.float:>3.1f}, {c.b.float:>3.1f}"
+      .replace(" ", "&nbsp;")
+  func htmlize(c: HSL): string =
+    fmt"{c.h.float:>5.1f}°, {c.s.float:>3.1f}, {c.l.float:>3.1f}"
+      .replace(" ", "&nbsp;")
+  func htmlize(c: Lab): string =
+    fmt"{c.l.float:>5.1f}, {c.a:>5.1f}, {c.b:>5.1f}".replace(" ", "&nbsp;")
 
   var
     appCalculator = document.getElementById("app-calculator")
